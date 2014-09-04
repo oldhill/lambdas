dca503c86d488bc7d7e3ea359ffae8bcae1d7367
oldhill <oldhillcode@gmail.com>
2014-09-03 23:02:55 -0700

    Try a few more basic examples

--- yo.py
+++ yo.py
@@ -1,3 +1,5 @@
+# Some basic lambda exploration
+
 def add_two(x):
   return x + 2
 
@@ -5,3 +7,5 @@ add_two_lambda = lambda x: x + 2
 
 print add_two(1)
 print add_two_lambda(1)
+print (lambda x: x + 2)(1)
+print (lambda: 3)()
